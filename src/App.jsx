82bc723852c8ec34dd6e1f752077ccd0b96fb835
JuanDavidDV <juandadazav@gmail.com>
2024-10-30 23:20:46 -0400

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
 import BadgeRewards from "./components/BadgeRewards/BadgeRewards";
 
 function App () {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<BadgeRewards />} />
       </Routes>     
