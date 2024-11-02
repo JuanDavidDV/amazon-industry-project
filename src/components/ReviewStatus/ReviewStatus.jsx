688e5de4d133901b7551e67a5f06182e16f0794b
JuanDavidDV <juandadazav@gmail.com>
2024-11-02 16:50:37 -0400

    form created

---
+++ src/components/ReviewStatus/ReviewStatus.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+const ReviewStatus = ({ reviewId }) => {
+  const [verificationStatus, setVerificationStatus] = useState(null);
+
+  
+
+  return (
+   <section>
+
+   </section>   
+  )
+}
+
+export default ReviewStatus;
