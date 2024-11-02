23c0cd6697200ebf1040f1053df2ba6a54315953
JuanDavidDV <juandadazav@gmail.com>
2024-11-02 17:27:17 -0400

    userhome page component created

---
+++ src/components/UserHomePage/UserHomePage.jsx
@@ -0,0 +1,14 @@
+const UserHomePage = () => {
+  return (
+    <section className="home-page">
+      <div className="home-page__container">
+        <div className="home-page">
+
+        </div>
+
+      </div>
+    </section>
+  )
+}
+
+export default UserHomePage;
